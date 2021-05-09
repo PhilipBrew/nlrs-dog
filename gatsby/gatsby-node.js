4551fb241023070bd82c01fa59962ccf5170a805
Phil Brew <phil.brew@cef.co.uk>
2021-05-09 16:42:37 +0100

    removed users pages

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,37 +1,9 @@
-import path from 'path';
-
-const turnUsersIntoPages = async ({ graphql, actions }) => {
-  const userTemplate = path.resolve('./src/templates/SingleUser.js');
-  const { data } = await graphql(`
-    query {
-      allSanityUsers {
-        nodes {
-          slug {
-            current
-          }
-        }
-      }
-    }
-  `);
-
-  const {
-    allSanityUsers: { nodes: users },
-  } = data;
-
-  users.forEach(({ slug }) => {
-    actions.createPage({
-      path: `user/${slug.current}`,
-      component: userTemplate,
-      context: {
-        slug: slug.current,
-      },
-    });
-  });
-};
+import path from "path";
+xw;
 
 const turnPoliciesIntoPages = async ({ graphql, actions }) => {
-  const policyTemplate = path.resolve('./src/templates/PolicyPage.js');
-  const pages = ['terms-Conditions', 'privacy-policy', 'cookie-policy'];
+  const policyTemplate = path.resolve("./src/templates/PolicyPage.js");
+  const pages = ["terms-Conditions", "privacy-policy", "cookie-policy"];
   pages.forEach((page) => {
     actions.createPage({
       path: page,
@@ -44,8 +16,5 @@ const turnPoliciesIntoPages = async ({ graphql, actions }) => {
 };
 
 export const createPages = async (params) => {
-  await Promise.all([
-    turnUsersIntoPages(params),
-    turnPoliciesIntoPages(params),
-  ]);
+  await Promise.all([turnPoliciesIntoPages(params)]);
 };
