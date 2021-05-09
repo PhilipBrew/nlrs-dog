4f8deff501e508de1bbef3034d08bead8fef4787
Phil Brew <phil.brew@cef.co.uk>
2021-05-09 13:33:22 +0100

    Update

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -1,27 +1,12 @@
-import { graphql } from 'gatsby';
-import React from 'react';
-import Layout from './src/components/Layout';
+import React from "react";
+import Layout from "./src/components/Layout";
 
-import AppProvider from './src/providers/AppProvider';
+import AppProvider from "./src/providers/AppProvider";
 
 export const wrapPageElement = ({ element, props }) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
   <Layout {...props}>{element}</Layout>
 );
-
-export const SiteSettingsQuery = graphql`
-  query siteSettings {
-    allSanitySettings {
-      nodes {
-        name
-        code
-        termsEnabled
-        privacyEnabled
-        cookieEnabled
-      }
-    }
-  }
-`;
 
 export const wrapRootElement = ({ element }) => (
   <AppProvider>{element}</AppProvider>
