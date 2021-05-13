41853e83a403b641f136b39b47f38d94dca8ee4c
Phil Brew <phil.brew@cef.co.uk>
2021-05-13 21:43:44 +0100

    stash

--- gatsby/.cache/sync-requires.js
+++ gatsby/.cache/sync-requires.js
@@ -5,12 +5,12 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/philbrew/Documents/repos/nlrs-dog/gatsby/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/philbrew/Documents/repos/nlrs-dog/gatsby/src/pages/404.js"))),
   "component---src-pages-faq-js": hot(preferDefault(require("/Users/philbrew/Documents/repos/nlrs-dog/gatsby/src/pages/faq.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/philbrew/Documents/repos/nlrs-dog/gatsby/src/pages/index.js"))),
   "component---src-pages-login-js": hot(preferDefault(require("/Users/philbrew/Documents/repos/nlrs-dog/gatsby/src/pages/login.js"))),
   "component---src-pages-order-js": hot(preferDefault(require("/Users/philbrew/Documents/repos/nlrs-dog/gatsby/src/pages/order.js"))),
-  "component---src-templates-policy-page-js": hot(preferDefault(require("/Users/philbrew/Documents/repos/nlrs-dog/gatsby/src/templates/PolicyPage.js"))),
-  "component---src-templates-single-user-js": hot(preferDefault(require("/Users/philbrew/Documents/repos/nlrs-dog/gatsby/src/templates/SingleUser.js")))
+  "component---src-templates-policy-page-js": hot(preferDefault(require("/Users/philbrew/Documents/repos/nlrs-dog/gatsby/src/templates/PolicyPage.js")))
 }
 
